640c41e8595cb5f823462439fda01645b73d4f90
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-10 20:03:15 +0900

    ✨ Timerコンポーネントの作成

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useMemo, useState } from "react";
 import "./app.css";
 import { data } from "./data";
 import { Trivia } from "./components/Trivia";
+import { Timer } from "./components/Timer";
 
 const App = () => {
   const [questionNumber, setQuestionNumber] = useState(1);
@@ -44,7 +45,9 @@ const App = () => {
         ) : (
           <>
             <div className="top">
-              <div className="timer">30</div>
+              <div className="timer">
+                <Timer setStop={setStop} questionNumber={questionNumber} />
+              </div>
             </div>
             <div className="bottom">
               <Trivia
